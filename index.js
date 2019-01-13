67707810d14e8d8b5198a1d26c331de1af3ab4fe
John Mark Redding <johnmarkredding@gmail.com>
2019-01-13 20:38:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function BoardMember(name, homeState, training) {
   this.doCharity = function () {
     return "I like to help people.";
   };
+  this.doCharity = function () {
+    return "I like to help people.";
+  };
 }
